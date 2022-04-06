dfa94c41c52ba39417f4c46b7fc530bf5ac4884c
merijn <merijnsweep@gmail.com>
2022-04-06 16:05:25 +0200

    #14

--- constructiepagina.php
+++ constructiepagina.php
@@ -15,7 +15,7 @@
   <link rel="manifest" href="site.webmanifest">
   <link rel="apple-touch-icon" href="icon.png">
   <!-- Place favicon.ico in the root directory -->
-  <link> rel="favicon" href="img/favicon.ico">
+  <link rel="favicon" href="img/favicon.ico">
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
 
@@ -27,7 +27,26 @@
   <!-- Add your site or application content here -->
   <div class="form-group">
   <img src="img/favicon.ico" alt="Start image">
-  <p>Hello world! This is HTML5 Boilerplate.</p>
+  <p>Wat maakt onze achtbanen nou zo speciaal? Het is vanwege onze 
+    eerste USP die wij voor deze achtbaan hebben ontwikkeld 
+    is een nieuwe metaal messing die nog nooit eerder in deze 
+    industrie gebruikt is. 
+    Door een menging te maken van 69% staal, 20% nikkel, 
+    en 11% koper  hebben een nieuwe messing ontwikkeld die wij nu 
+    MagnaAlloy noemen. Met deze nieuwe MagnaAlloy maken wij een aantal 
+    onderdelen die de meeste stress ondergaan in de achtbaan, 
+    waardoor deze nog meer G-krachten kunnen weerstaan, en langer 
+    meegaan dan andere stalen. We hebben enorm geprobeerd de kosten 
+    zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 
+    8% duurder uitvallen 
+    an andere soortgelijke onderdelen, zonder kwaliteitsverlies.
+    . Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, 
+    dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt 
+    dit ervoor dat een aantal designs 
+    voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn.
+    Dus maakt het ook plaats voor nieuwere ontwerpen die zeker de aandacht 
+    op uw pretpark houdt.
+  </p>
   </div>
   <script src="js/vendor/modernizr-3.11.2.min.js"></script>
   <script src="js/plugins.js"></script>
