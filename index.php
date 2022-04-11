5c12c2e3c9ca075755713b5f45353a872a48399d
KC <93526857+Consiglieri-K@users.noreply.github.com>
2022-04-11 18:50:20 +0200

    Header, footer en main page gemaakt
    Fix #3 #10 #9 #2 #1 #12 #11

--- index.php
+++ index.php
@@ -18,14 +18,27 @@
 
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <meta name="theme-color" content="#fafafa">
 </head>
 
 <body>
 
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
+  <?php
+  require_once('header.php'); 
+  ?>
+  <main>
+    <div class="container">
+      <h1>Welkom bij de nieuwe achtbaan innovatie van SuperCoaster NV!  </h1>
+      <p>Supercoaster NV presenteert met trots het nieuwe type achtbaan op deze webpagina. Zie de navigatie voor de verschillende informatie.</p>
+    </div>
+    <div class="loopingImage">
+      `<img src="img/looping.png" alt="looping">
+    </div>
+  </main>
+  
   <script src="js/vendor/modernizr-3.11.2.min.js"></script>
   <script src="js/plugins.js"></script>
   <script src="js/main.js"></script>
@@ -36,6 +49,10 @@
     ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
   </script>
   <script src="https://www.google-analytics.com/analytics.js" async></script>
+
+  <?php
+  require_once('footer.php'); 
+  ?>
 </body>
 
 </html>
